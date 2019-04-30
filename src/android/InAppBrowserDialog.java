fd12564d211041211eebc3f82b150e960a0d867c
rogerben <40767658+rogerben@users.noreply.github.com>
2019-05-01 06:07:54 +0800

    Update InAppBrowserDialog.java

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -31,7 +31,7 @@ public class InAppBrowserDialog extends Dialog {
     InAppBrowser inAppBrowser = null;
 
     public InAppBrowserDialog(Context context, int theme) {
-        super(context, theme);
+        super(context, R.style.full_screen_dialog);
         this.context = context;
     }
 
