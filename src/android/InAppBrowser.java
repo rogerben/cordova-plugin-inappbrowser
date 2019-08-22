91c2a35d516a64296a8e3b3ec6b9a2023ffa9490
rogerben <40767658+rogerben@users.noreply.github.com>
2019-08-22 14:02:14 +0800

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -325,7 +325,7 @@ public class InAppBrowser extends CordovaPlugin {
     @Override
     public void onResume(boolean multitasking) {
            
-           dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+           dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
            dialog.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
            
         if (shouldPauseInAppBrowser) {
